c23f94e01677548f58f297b5fea5543dcf8ca55c
Dhanashri <dhanasp@thoughtworks.com>
2024-10-21 15:34:10 +0530

    Added instruction to give the input added support for passing arguments to the main method.

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Chess-assignment
+I have written this program in kotlin to solve the chess assignment.
+Currently, the program is able to calculate the moves of the pawn, king, and queen.
+
+To run the program, run the following command in the terminal:
+
+```bash
+./gradlew clean build
+./gradlew run --args="<Piece> <Position>"
+   example: ./gradlew run --args="Pawn D4"
+```
+
+To run the test cases, run the following command in the terminal:
+
+```bash
+./gradlew clean test
+```
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,10 +3,9 @@ package org.example.game
 import org.example.game.models.ChessPiece
 import org.example.game.models.Game
 
-fun main() {
-
-    val currentPosition = "G1"
-    val chessPiece = ChessPiece.PAWN
+fun main(args: Array<String>) {
+    val chessPiece = ChessPiece.valueOf(args[0].uppercase())
+    val currentPosition = args[1]
     val movements = Game().getAllPossibleMovesFor(chessPiece, currentPosition)
     println("Possible movements for ${chessPiece.name} at $currentPosition: $movements")
 }
